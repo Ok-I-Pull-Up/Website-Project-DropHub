6637cd3885dc6fa8cb32edd09225bd22442e9827
Fresh <Fr3sh.qq@gmail.com>
2023-07-21 12:10:02 +0200

    some style changes

--- js/main.js
+++ js/main.js
@@ -22,15 +22,22 @@ const handleScrollSpy = () => {
 			if (window.scrollY <= section.offsetTop + section.offsetHeight - 96) {
 				sections.push(section);
 
-				const activeSection = document.querySelector(`[href*="${sections[0].id}"]`);
+				const activeSection = document.querySelector(
+					`[href*="${sections[0].id}"]`
+				);
 
 				menuItems.forEach((item) => item.classList.remove('active'));
 
 				activeSection.classList.add('active');
 			}
 
-			if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 180) {
-				const lastSection = document.querySelector('.navbar__menu a:last-of-type');
+			if (
+				window.innerHeight + window.scrollY >=
+				document.body.offsetHeight - 180
+			) {
+				const lastSection = document.querySelector(
+					'.navbar__menu a:last-of-type'
+				);
 
 				menuItems.forEach((item) => item.classList.remove('active'));
 
